dc756953f70ea87fc32ddafa759642c9af705d76
Er. Nitish Kamat <nitishkkamat08@gmail.com>
2025-07-18 16:07:27 +0530

    Added gunicorn to requirements

--- mobile_opt/opt_app/urls.py
+++ mobile_opt/opt_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.homepage, name='base'),
     path('send-otp/', views.send_otp_view, name='send_otp'),
     path('verify-otp/', views.verify_otp_view, name='verify_otp'),
 ]
